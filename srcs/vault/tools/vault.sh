f179d55fa71a57619ab6e5a88ba43902fbf78c21
Ivanna Tchinda <91744423+ivanna-tchinda@users.noreply.github.com>
2024-04-22 18:27:17 +0200

    Add files via upload

--- srcs/vault/tools/vault.sh
+++ srcs/vault/tools/vault.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Attendre que Vault soit prêt
-while ! vault status > /dev/null 2>&1; do
-    sleep 1
-done
 
-# Exécuter d'autres commandes une fois que Vault est prêt
-echo "Vault is ready. Executing further commands..."
-
-# Exemple : Exécutez une commande après que Vault soit prêt
+# Exécuter la commande spécifiée dans CMD
